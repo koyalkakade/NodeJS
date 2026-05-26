bcafa3dbacdf6fadbca66455a7f9e1dd0cd5d5e6
Koyal Nimbalkar <koyalkakade@gmail.com>
2026-05-26 16:38:12 +0530

    added more api

--- EMP-MGMT/controllers/empController.js
+++ EMP-MGMT/controllers/empController.js
@@ -17,7 +17,7 @@ const emp = [
         "address": "45 Nehru Nagar, Mumbai, Maharashtra",
         "contactNumber": "9123456789",
         "dateOfBirth": "1988-11-22",
-        "joiningDate": "2018-01-10",
+        "joiningDate": "2013-09-16",
         "department": "IT"
     },
     {
@@ -37,7 +37,7 @@ const emp = [
         "address": "23 Connaught Place, New Delhi",
         "contactNumber": "9765432109",
         "dateOfBirth": "1985-02-28",
-        "joiningDate": "2014-03-20",
+        "joiningDate": "2020-11-05",
         "department": "Marketing"
     },
     {
@@ -47,8 +47,18 @@ const emp = [
         "address": "56 Anna Salai, Chennai, Tamil Nadu",
         "contactNumber": "9345678901",
         "dateOfBirth": "1993-05-12",
-        "joiningDate": "2020-11-05",
+        "joiningDate": "2017-10-05",
         "department": "Operations"
+    },
+    {
+        "id": 6,
+        "name": "Alice",
+        "email": "kavya.iyer@company.com",
+        "address": "56 Anna Salai, Chennai, Tamil Nadu",
+        "contactNumber": "9345678901",
+        "dateOfBirth": "1993-05-12",
+        "joiningDate": "2017-10-05",
+        "department": "HR"
     }
 ];
 
@@ -135,15 +145,15 @@ const getDepartmentWiseEmployee = (req, res) => {
 
     try {
         const result = emp.filter((e) => {
-           return e.department.toLowerCase() === department.toLowerCase()
+            return e.department.toLowerCase() === department.toLowerCase()
         }
         )
         // console.log(result)
         if (result.length > 0) {
             res.status(200).send({ departmentWiseEmp: result })
         }
-        else{
-             res.status(203).send({ departmentWiseEmp: "Data not found" })
+        else {
+            res.status(203).send({ departmentWiseEmp: "Data not found" })
         }
 
     } catch (error) {
@@ -151,22 +161,22 @@ const getDepartmentWiseEmployee = (req, res) => {
     }
 }
 
-const getJoiningMothWiseEmp=(req,res)=>{
-      const month = req.query.month
+const getJoiningMothWiseEmp = (req, res) => {
+    const month = req.query.month
 
     try {
         const result = emp.filter((e) => {
             // console.log(new Date(e.joiningDate).getMonth()+1)
-           const joiningMonth= new Date(e.joiningDate).getMonth()+1
-           return joiningMonth==month
+            const joiningMonth = new Date(e.joiningDate).getMonth() + 1
+            return joiningMonth == month
         }
         )
         // console.log(result)
         if (result.length > 0) {
             res.status(200).send({ monthtWiseEmp: result })
         }
-        else{
-             res.status(203).send({ monthtWiseEmp: "Data not found" })
+        else {
+            res.status(203).send({ monthtWiseEmp: "Data not found" })
         }
 
     } catch (error) {
@@ -174,22 +184,22 @@ const getJoiningMothWiseEmp=(req,res)=>{
     }
 }
 
-const getThisMonthBirthdayEmp=(req,res)=>{
-      const currentMonth= new Date().getMonth()+1
-      // console.log('currentMonth- ',currentMonth)
+const getThisMonthBirthdayEmp = (req, res) => {
+    const currentMonth = new Date().getMonth() + 1
+    // console.log('currentMonth- ',currentMonth)
     try {
         const result = emp.filter((e) => {
-           const birthMonth= new Date(e.dateOfBirth).getMonth()+1
-          //  console.log('birthMonth- ',currentMonth)
-           return birthMonth===currentMonth
+            const birthMonth = new Date(e.dateOfBirth).getMonth() + 1
+            //  console.log('birthMonth- ',currentMonth)
+            return birthMonth === currentMonth
         }
         )
         // console.log(result)
         if (result.length > 0) {
             res.status(200).send({ ThisMonthtBirthdayEmp: result })
         }
-        else{
-             res.status(203).send({ ThisMonthtBirthdayEmp: "Data not found" })
+        else {
+            res.status(203).send({ ThisMonthtBirthdayEmp: "Data not found" })
         }
 
     } catch (error) {
@@ -198,7 +208,204 @@ const getThisMonthBirthdayEmp=(req,res)=>{
 
 }
 
+const getEmployeeByName = (req, res) => {
+    const name = req.query.name
 
+    try {
+        const result = emp.filter((e) => {
+            return e.name.toLowerCase() === name.toLowerCase()
+            //  console.log(e.name.toLowerCase())
+            //   console.log(name.toLowerCase())
+        }
+        )
+        // console.log(result)
+        if (result.length > 0) {
+            res.status(200).send({ EmployeeByName: result })
+        }
+        else {
+            res.status(203).send({ EmployeeByName: "Data not found" })
+        }
 
-module.exports = { getAllEmp, createEmp, deleteEmp, updateEmp, getEmpById,
-     getDepartmentWiseEmployee,getJoiningMothWiseEmp,getThisMonthBirthdayEmp }
+    } catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const getEmployeeByCity = (req, res) => {
+    const city = req.query.city
+    //console.log(city)
+    try {
+        const result = emp.filter((e) => {
+            return e.address.toLowerCase() === city.toLowerCase()
+        }
+        )
+        // console.log(result)
+        if (result.length > 0) {
+            res.status(200).send({ EmployeeByCity: result })
+        }
+        else {
+            res.status(203).send({ EmployeeByCity: "Data not found" })
+        }
+
+    } catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const sortByJoiningDate = (req, res) => {
+    try {
+        const sortEmp = emp.sort((a, b) => new Date(a.joiningDate) - new Date(b.joiningDate));
+        if (sortEmp.length > 0) {
+            res.status(200).send({ sortByJoiningDate: sortEmp })
+        }
+        else {
+            res.status(203).send({ sortByJoiningDate: "Data not found" })
+        }
+
+    }
+    catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const sortByName = (req, res) => {
+    try {
+        const sortEmpByName = emp.sort((a, b) => a.name.localeCompare(b.name))
+        if (sortEmpByName.length > 0) {
+            res.status(200).send({ sortEmpByName: sortEmpByName })
+        }
+        else {
+            res.status(203).send({ sortEmpByName: "Data not found" })
+        }
+
+    }
+    catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const getAllEmpCount = (req, res) => {
+    try {
+        const count = emp.length
+        if (emp.length > 0) {
+            res.status(200).send({ EmpCount: count })
+        }
+        else {
+            res.status(203).send({ EmpCount: "Data not found" })
+        }
+
+    }
+    catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const getTotalDepartmentWise = (req, res) => {
+    try {
+        const depWiseCount = emp.reduce((acc, e) => {
+            acc[e.department] = (acc[e.department] || 0) + 1
+            return acc
+        }, {})
+        //console.log(depWiseCount);
+        if (!depWiseCount) {
+            res.status(203).send({ depWiseCount: "Data not found" })
+        }
+        else {
+            res.status(200).send({ depWiseCount: depWiseCount })
+        }
+
+    }
+    catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const getOldestEmp = (req, res) => {
+    try {
+        const oldestEmp = emp.reduce((prev, current) => {
+            return (new Date(prev.dateOfBirth) < new Date(current.dateOfBirth)) ? prev : current;
+        })
+        //console.log(depWiseCount);
+        if (!oldestEmp) {
+            res.status(203).send({ oldestEmp: "Data not found" })
+        }
+        else {
+            res.status(200).send({ oldestEmp: oldestEmp })
+        }
+
+    }
+    catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const getNewestJoinedEmp = (req, res) => {
+    try {
+        const newestJoinedEmp = emp.reduce((prev, current) => {
+            return (new Date(prev.joiningDate) < new Date(current.joiningDate)) ? current : prev;
+        })
+        //console.log(depWiseCount);
+        if (!newestJoinedEmp) {
+            res.status(203).send({ newestJoinedEmp: "Data not found" })
+        }
+        else {
+            res.status(200).send({ newestJoinedEmp: newestJoinedEmp })
+        }
+
+    }
+    catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const filterEmployeeTwoJoiningDates = (req, res) => {
+    const start = req.query.start
+    const end = req.query.end
+    //console.log(city)
+    try {
+        const result = emp.filter((e) => {
+            return new Date(e.joiningDate) >= new Date(start) && new Date(e.joiningDate) <= new Date(end)
+        }
+        )
+        // console.log(result)
+        if (result.length > 0) {
+            res.status(200).send({ EmployeeByCity: result })
+        }
+        else {
+            res.status(203).send({ EmployeeByCity: "Data not found" })
+        }
+
+    } catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+const updateAllEmpDetails = (req, res) => {
+    const { ID } = req.params;
+    const { name, email, address, contactNumber, joiningDate, dateOfBirth, department } = req.body
+    try {
+        const index = emp.findIndex((e) => e.id == ID)
+        if (index == -1) {
+            res.status(400).send({ msg: "employee not found" })
+        }
+        emp[index].name = name
+        emp[index].email = email
+        emp[index].address = address
+        emp[index].contactNumber = contactNumber
+        emp[index].joiningDate = joiningDate
+        emp[index].dateOfBirth = dateOfBirth
+        emp[index].department = department
+        res.status(200).send({ msg: "employee updated secessfully" })
+
+    } catch (error) {
+        res.status(500).send({ msg: "server error" })
+    }
+}
+
+module.exports = {
+    getAllEmp, createEmp, deleteEmp, updateEmp, getEmpById,
+    getDepartmentWiseEmployee, getJoiningMothWiseEmp, getThisMonthBirthdayEmp,
+    getEmployeeByName, getEmployeeByCity, sortByJoiningDate, sortByName, getAllEmpCount,
+    getTotalDepartmentWise, getOldestEmp, getNewestJoinedEmp, filterEmployeeTwoJoiningDates,
+    updateAllEmpDetails
+}
--- EMP-MGMT/routes/empRoute.js
+++ EMP-MGMT/routes/empRoute.js
@@ -18,4 +18,24 @@ router.get('/month/search',empController.getJoiningMothWiseEmp)
 
 router.get('/getThisMonthBirthdayEmp',empController.getThisMonthBirthdayEmp)
 
+router.get('/name/search',empController.getEmployeeByName)
+
+router.get('/city/search',empController.getEmployeeByCity)
+
+router.get('/sortByJoiningDate',empController.sortByJoiningDate)
+
+router.get('/sortByName',empController.sortByName)
+
+router.get('/getAllEmpCount',empController.getAllEmpCount)
+
+router.get('/getTotalDepartmentWise',empController.getTotalDepartmentWise)
+
+router.get('/getOldestEmp',empController.getOldestEmp)
+
+router.get('/getNewestJoinedEmp',empController.getNewestJoinedEmp)
+
+router.get('/filter',empController.filterEmployeeTwoJoiningDates)
+
+router.patch('/updateAllEmpDetails/:ID',empController.updateAllEmpDetails)
+
 module.exports=router
